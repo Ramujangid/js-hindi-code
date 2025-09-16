39362fb588bfec05a271d2489add1e6f1f76068c
RK JANGID <164884836+Ramujangid@users.noreply.github.com>
2025-09-16 18:59:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "rrrr"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "22" => 22
+// "2sds"=> NAN
+// true => 1; false => 0
+
+let isloggedIn = "Ram"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Ram" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); // 33
+console.log(typeof stringNumber);  // string
