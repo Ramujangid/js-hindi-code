35d54db0751f0a00191a74e2e2debaecfac9ae7b
RK JANGID <164884836+Ramujangid@users.noreply.github.com>
2025-09-17 17:04:00 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,34 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 let someNumber = "33"
 
 let stringNumber = String(someNumber)
-console.log(stringNumber); // 33
-console.log(typeof stringNumber);  // string
+// console.log(stringNumber); // 33
+// console.log(typeof stringNumber);  // string
+
+// ********************Operation*********************
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Ramesh"
+let str2 = "war"
+let str3 = str1 + str2
+
+console.log((3 + 4) * (5 % 3)); // 14
+console.log(3 + 4 * 5 % 3);  // 5
+
+console.log(true) // true
+console.log(+true) // 1
+
+console.log(+"")
+
+let
