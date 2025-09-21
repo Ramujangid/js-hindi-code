a8bc38984a06402aa8e370cabab81831034a4436
RK JANGID <164884836+Ramujangid@users.noreply.github.com>
2025-09-21 19:31:00 +0000

    datatypes with function

--- Datatypes-summery.js
+++ Datatypes-summery.js
@@ -1,7 +1,38 @@
 // primitive
-
 // 7 types : String, Number, Boolean , null, undefied, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // false
+
+const bigNumber = 32322322323266566656n // bigInt
 
 // Reference (non primitive)
 
 // Array, Objects, Functions
+const heros = ["shaktiman","naagraj", "doga"];
+let myObj = {
+    name: "Rameshwar",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof myFunction); // function
+console.log(typeof bigNumber); // bigint
+console.log(typeof scoreValue); // number
+
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof outsideTemp); // object
+
+console.log(typeof anotherId); // symbol
+console.log(typeof userEmail); // undefined
