c6799b192a9437cb14f3d3e8d4a52e6ff0ecfe31
RK JANGID <164884836+Ramujangid@users.noreply.github.com>
2025-09-26 18:52:27 +0000

    datatypes summery

--- Datatypes-summery.js
+++ Datatypes-summery.js
@@ -27,12 +27,36 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello world");
 }
-console.log(typeof myFunction); // function
-console.log(typeof bigNumber); // bigint
-console.log(typeof scoreValue); // number
+//console.log(typeof myFunction); // function
+//console.log(typeof bigNumber); // bigint
+//console.log(typeof scoreValue); // number
 
-console.log(typeof isLoggedIn); // boolean
-console.log(typeof outsideTemp); // object
+//console.log(typeof isLoggedIn); // boolean
+//console.log(typeof outsideTemp); // object
 
-console.log(typeof anotherId); // symbol
-console.log(typeof userEmail); // undefined
+//console.log(typeof anotherId); // symbol
+//console.log(typeof userEmail); // undefined
+
+//*******************************************************
+
+// Stack (primitive), Heap (non-primitive)
+
+let myYoutubename = "rk jangid com"
+
+let anothername = myYoutubename
+anothername = "Ram jangid official"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "rkjangra@google.com",
+    upi: "rkjangra@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "raju@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
